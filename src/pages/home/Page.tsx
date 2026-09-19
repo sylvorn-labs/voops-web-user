d462ecd1301b0b0b8a39ce9e23e64ea5f1558d07
Jenil Desai <jenildev91@gmail.com>
2026-09-19 13:42:47 +0530

    feat(home): add pricing section

--- src/pages/home/Page.tsx
+++ src/pages/home/Page.tsx
@@ -7,6 +7,7 @@ import { TrustStrip } from './components/TrustStrip';
 import { FeaturesMatrix } from './components/FeaturesMatrix';
 import { FeatureCardsGrid } from './components/FeatureCardsGrid';
 import { Testimonials } from './components/Testimonials';
+import { Pricing } from './components/Pricing';
 import { Team } from './components/Team';
 import { Faq } from './components/Faq';
 import { Download } from './components/Download';
@@ -25,6 +26,7 @@ export function HomePage() {
         <FeaturesMatrix />
         <FeatureCardsGrid />
         <Testimonials />
+        <Pricing />
         <Team />
         <Faq />
         <Download />
